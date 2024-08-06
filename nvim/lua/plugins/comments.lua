f026322146a69966f9576a0386d954959ad0aeb0
Ammiel7258 <ammiel.maynard@gmail.com>
2024-08-06 17:34:40 -0400

    comments working with leader cc

--- nvim/lua/plugins/comments.lua
+++ nvim/lua/plugins/comments.lua
@@ -1,6 +1,11 @@
 return {
-	{
-		"numToStr/Comment.nvim",
-		opts = {},
-	},
+  {
+    "numToStr/Comment.nvim",
+    opts = {},
+    config = function()
+      require("Comment").setup()
+
+      vim.keymap.set("n", "<leader>cc", '<cmd>lua require("Comment.api").toggle.linewise.current()<CR>', {})
+    end,
+  },
 }
---
+++ nvim/lua/plugins/ufo.lua
@@ -0,0 +1,10 @@
+return {
+	"kevinhwang91/nvim-ufo",
+	requires = {
+		"kevinhwang91/promise-async",
+	},
+
+	config = function()
+		-- figure out how to set this up
+	end,
+}
