36d952c6289c720db17f508df92c7d6d2ad49c30
Ammiel7258 <ammiel.maynard@gmail.com>
2024-08-06 16:37:35 -0400

    reorder

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -19,13 +19,13 @@ return {
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
 
+      lspconfig.csharp_ls.setup({
+        capabilities = capabilities,
+      })
       lspconfig.gopls.setup({
         capabilities = capabilities,
       })
       lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.csharp_ls.setup({
         capabilities = capabilities,
       })
       lspconfig.pyright.setup({
